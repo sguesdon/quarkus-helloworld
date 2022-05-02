55aca3ef3b74728fc0a967cab6535a2b2f6dfbc3
Sebastian Guesdon <sebastian.guesdon@gmail.com>
2022-05-02 21:15:14 +0200

    init

---
+++ src/test/java/org/sguesdon/poc/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.sguesdon.poc;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
